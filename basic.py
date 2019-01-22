9f30bd1113784fb61d821f5c01d30669075a88dd
Douglas Bacon <dugbdev@gmail.com>
2019-01-21 19:44:08 -0500

    car on screen

--- basic.py
+++ basic.py
@@ -1,9 +1,27 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+
+display_width = 800
+display_height = 600
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
+carImg = pygame.image.load('racecar.png')
+
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+x=(display_width * 0.45)
+y=(display_height * 0.8)
+
 
 crashed = False
 
@@ -12,11 +30,10 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-        print(event)
-
+    gameDisplay.fill(white)
+    car(x,y)
     pygame.display.update()
-
-    clock.tick(60) #FPS
+    clock.tick(60)  # FPS
 
 pygame.quit()
 quit()
