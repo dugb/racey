811c26b53dcf1c39dab72163021a817ddd8c0a95
Douglas Bacon <dugbdev@gmail.com>
2019-01-23 17:51:40 -0500

    added edge boundaries

--- racey.py
+++ racey.py
@@ -11,6 +11,8 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+car_width = 73
+
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
@@ -20,36 +22,42 @@ carImg = pygame.image.load('racecar.png')
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
+def game_loop():
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
-change_x = 0
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    change_x = 0
+
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    change_x += -5
+                if event.key == pygame.K_RIGHT:
+                    change_x += 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    change_x += 5
+                if event.key == pygame.K_RIGHT:
+                    change_x += -5
+
+        x += change_x
+
+        gameDisplay.fill(white)
+        car(x, y)
+
+        if x > (display_width - car_width) or x <0:
+            gameExit = True
+
+        pygame.display.update()
+        clock.tick(60)  # FPS
 
 
-crashed = False
-
-while not crashed:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                change_x += -5
-            if event.key == pygame.K_RIGHT:
-                change_x += 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                change_x += 5
-            if event.key == pygame.K_RIGHT:
-                change_x += -5
-
-    x += change_x
-
-    gameDisplay.fill(white)
-    car(x, y)
-    pygame.display.update()
-    clock.tick(60)  # FPS
-
+game_loop()
 pygame.quit()
 quit()
