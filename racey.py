16d1315e8cb364d3e8c5d5aed404a2fabfd76a10
Douglas Bacon <dugbdev@gmail.com>
2019-01-30 17:47:17 -0500

    added crash

--- racey.py
+++ racey.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -22,8 +23,28 @@ carImg = pygame.image.load('racecar.png')
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
-def game_loop():
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2), (display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display('You Crashed!')
+
+
+def game_loop():
+    pygame.event.clear()
     x = (display_width * 0.45)
     y = (display_height * 0.8)
     change_x = 0
@@ -34,7 +55,8 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     change_x += -5
@@ -52,7 +74,7 @@ def game_loop():
         car(x, y)
 
         if x > (display_width - car_width) or x <0:
-            gameExit = True
+            crash()
 
         pygame.display.update()
         clock.tick(60)  # FPS
