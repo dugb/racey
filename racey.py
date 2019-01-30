067403a2bd96d12616dcd6a620301be2931133da
Douglas Bacon <dugbdev@gmail.com>
2019-01-30 18:05:00 -0500

    add avoid blocks

--- racey.py
+++ racey.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -20,11 +21,16 @@ clock = pygame.time.Clock()
 carImg = pygame.image.load('racecar.png')
 
 
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
+
 def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+    textSurface = font.render(text, True, red)
     return textSurface, textSurface.get_rect()
 
 def message_display(text):
@@ -32,12 +38,10 @@ def message_display(text):
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2), (display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
-
     pygame.display.update()
-
     time.sleep(2)
-
     game_loop()
+
 
 def crash():
     message_display('You Crashed!')
@@ -48,6 +52,13 @@ def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
     change_x = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed  = 7
+    thing_width  = 100
+    thing_height = 100
+
 
     gameExit = False
 
@@ -71,10 +82,18 @@ def game_loop():
         x += change_x
 
         gameDisplay.fill(white)
+
+        # things(thingx, thingy, thingw, thingh, color):
+        things(thing_startx, thing_starty, thing_width, thing_height, blue)
+        thing_starty += thing_speed
         car(x, y)
 
         if x > (display_width - car_width) or x <0:
             crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
 
         pygame.display.update()
         clock.tick(60)  # FPS
